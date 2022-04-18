932d80bca26a5d2158f4b1531b0cab25ca072ccd
Arun Kumar <45923721+bytesbit@users.noreply.github.com>
2022-04-18 16:13:53 +0530

    Update install.sh

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -1,41 +1,41 @@
-!/bin/bash
-echo "Here is the env varialbe : $1"
-# set -e
+#!/bin/bash
 
-# cd ~ || exit
+set -e
 
-# sudo apt-get -y install redis-server libcups2-dev -qq
+cd ~ || exit
 
-# pip install frappe-bench
+sudo apt-get -y install redis-server libcups2-dev -qq
 
-# git clone https://github.com/frappe/frappe --branch version-13 --depth 1
-# bench init --skip-assets --frappe-path ~/frappe --python 3.8.12 frappe-bench
+pip install frappe-bench
 
-# mkdir ~/frappe-bench/sites/test_site
-# cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites/test_site/
+git clone https://github.com/frappe/frappe --branch version-13 --depth 1
+bench init --skip-assets --frappe-path ~/frappe --python 3.8.12 frappe-bench
 
-# mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
-# mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
+mkdir ~/frappe-bench/sites/test_site
+cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites/test_site/
 
-# mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
-# mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe"
-# mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'"
+mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
+mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
 
-# mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'"
-# mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
+mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
+mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe"
+mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'"
 
-# cd ~/frappe-bench || exit
+mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'"
+mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
 
-# sed -i 's/watch:/# watch:/g' Procfile
-# sed -i 's/schedule:/# schedule:/g' Procfile
-# sed -i 's/socketio:/# socketio:/g' Procfile
-# sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
+cd ~/frappe-bench || exit
 
-# bench get-app erpnext --branch version-13
+sed -i 's/watch:/# watch:/g' Procfile
+sed -i 's/schedule:/# schedule:/g' Procfile
+sed -i 's/socketio:/# socketio:/g' Procfile
+sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-# bench start &
-# bench --site test_site reinstall --yes
+bench get-app erpnext --branch version-13
 
-# bench get-app proshanti_lms git@github.com:true-value-access/proshanti-lms.git
-# bench --site test_site install-app proshanti_lms 
-# bench setup requirements --dev
+bench start &
+bench --site test_site reinstall --yes
+
+bench get-app proshanti_lms --branch $1 git@github.com:true-value-access/proshanti-lms.git
+bench --site test_site install-app proshanti_lms 
+bench setup requirements --dev
